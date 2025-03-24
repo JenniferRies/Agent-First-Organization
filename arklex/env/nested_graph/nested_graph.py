3b0fba0f98cb95be9f689e80cfa96f51d4b05836
luigi617 <luigiliu617@gmail.com>
2025-03-24 17:44:33 -0400

    fix

--- arklex/env/nested_graph/nested_graph.py
+++ arklex/env/nested_graph/nested_graph.py
@@ -8,10 +8,10 @@ class NestedGraph:
         self.value = value
     
     def get_nested_graph_start_node_id(self):
-        return self.value
+        return str(self.value)
     
     @classmethod
-    def get_nested_graph_component_node(self, params, nodes):
+    def get_nested_graph_component_node(self, params):
         path = params["path"]
         cur_node_i = len(path) - 1
         prev_node_id = None
@@ -20,13 +20,34 @@ class NestedGraph:
                 cur_node_i = path[cur_node_i]["nested_graph_jump"]
             cur_node = path[cur_node_i]
             cur_node_id = cur_node["node_id"]
-            cur_node_value = nodes[cur_node_id]["attribute"]["value"]
-            if cur_node_value == prev_node_id:
-                params["path"][-1]["nested_graph_jump"] = cur_node_i
+            to_node_id = cur_node.get("to_node_id", None)
+            if to_node_id is not None and to_node_id == prev_node_id:
                 return cur_node_id, params
             
             prev_node_id = cur_node_id
             cur_node_i -= 1
         
-        return path[-1]["node_id"], params
+        return None, params
+    
+    @classmethod
+    def set_last_node_jump_if_leaf(self, params):
+        if not params["path"][-1]["is_leaf"]:
+            return params
+        path = params["path"]
+        cur_node_i = len(path) - 1
+        prev_node_id = None
+        while cur_node_i >= 0:
+            if "nested_graph_jump" in path[cur_node_i]:
+                cur_node_i = path[cur_node_i]["nested_graph_jump"]
+            cur_node = path[cur_node_i]
+            cur_node_id = cur_node["node_id"]
+            to_node_id = cur_node.get("to_node_id", None)
+            if to_node_id is not None and to_node_id == prev_node_id:
+                params["path"][-1]["nested_graph_jump"] = cur_node_i
+                return params
+            
+            prev_node_id = cur_node_id
+            cur_node_i -= 1
+        return params
+        
 
